7dc85268984e75213a692794d273f0a42413eb04
Babos Kristóf <69733878+baboskristof@users.noreply.github.com>
2022-12-03 14:40:54 +0100

    Remove gitignored dir [build]

--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: P:/mosze-semester-project-gitanos/build/_deps/googletest-src
-# Build directory: P:/mosze-semester-project-gitanos/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
